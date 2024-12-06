ef95212b84a2e73b697d6e63b0d5a036fbd2f27d
RobsenGelana <robsengelana21@gmail.com>
2024-12-06 15:56:43 +0300

    Fixed bug

--- login.js
+++ login.js
@@ -1,1 +1,19 @@
 "use strict"
+
+const inputEmail = document.querySelector(".email");
+const inputPassword = document.querySelector(".password");
+const btnLogin = document.querySelector(".signin");
+
+
+
+let userDetail = {
+    "email":"",
+    "password":""
+}
+
+btnLogin.addEventListener("click", function(e){
+    e.preventDefault()
+    userDetail.email = inputEmail.value;
+    userDetail.password = inputPassword.value
+})
+

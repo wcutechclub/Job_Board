b0d5f8062dfd5e8d4e16aa45684b93b6df2fb755
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2024-12-23 14:15:32 +0300

    create the object for the input variables

--- signupseeker.js
+++ signupseeker.js
@@ -1,16 +1,39 @@
-"use strict"
+"use strict";
 
-const fnameLable = document.querySelector(".fname");
-const lnameLable = document.querySelector(".lname");
-const emailLable = document.querySelector(".email");
-const dropdown = document.getElementById("options");
-const selectedGender = document.querySelector('input[name="gender"]:checked');
-const profileLink = document.querySelector(".profile");
-const resumeLink = document.querySelector(".resume");
+const form = document.querySelector(".form");
+
+// Inputs
+const inputFirstName = document.querySelector(".fname");
+const inputLastName = document.getElementById("last-name");
+const inputEmail = document.querySelector(".email");
 const passwordInput = document.querySelector(".password");
 const confirmPasswordInput = document.querySelector(".confirm_password");
-const userBioTextArea = document.getElementById("bio");
+const inputResumeLink = document.querySelector(".resume");
 
+///////////////////////////////////////////////////////
+const industryType = document.querySelector(".industry-type");
+const genderInput = document.querySelector(".gender-input");
 
+const inputProfileLink = document.querySelector(".profile");
+const inputBio = document.getElementById("bio");
 
+const btn = document.querySelector(".signup_btn");
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (confirmPasswordInput.value === passwordInput.value) {
+    const signUpData = {
+      first_name: inputFirstName.value,
+      last_name: inputLastName.value,
+      email: inputEmail.value,
+      gender: genderInput.value,
+      password: passwordInput.value,
+      bio: inputBio.value,
+      job_category: industryType.value,
+      resume_url: inputResumeLink.value,
+      profile_pic_url: inputProfileLink.value,
+      user_type: "JF",
+    };
+    console.log(signUpData);
+  }
+});

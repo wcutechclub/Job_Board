7e0e0764e10f3814c3a0cbc9916da7378d1e9670
Samuel-Tefera <samuelteferab@gmail.com>
2024-12-24 12:39:29 +0300

    handle job finder sign up request

--- js/signupseeker.js
+++ js/signupseeker.js
@@ -19,9 +19,9 @@ const inputBio = document.getElementById("bio");
 
 const btn = document.querySelector(".signup_btn");
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", async function (e) {
   e.preventDefault();
-  if (confirmPasswordInput.value === passwordInput.value) {
+  if ( confirmPasswordInput.value === passwordInput.value ) {
     const signUpData = {
       first_name: inputFirstName.value,
       last_name: inputLastName.value,
@@ -35,5 +35,27 @@ form.addEventListener("submit", function (e) {
       user_type: "JF",
     };
     console.log(signUpData);
+    
+    try {
+      const response = await fetch( 'http://localhost:8000/user/create/', {
+        method: 'POST',
+        body: JSON.stringify( signUpData ),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      } );
+
+      if ( response.ok ) {
+        const data = await response.json();
+        console.log("Sign up Successful: ", data);
+        window.location.href = "/login.html";
+      } else {
+        const errorData = await response.json();
+        alert("Sign Up failed: "+ errorData.detail || "Please check your signup form.")
+      }
+    } catch(error) {
+      console.log("Error: ", error);
+      alert("An unexpected error occurred. Please try again later.")
+    }
   }
 });

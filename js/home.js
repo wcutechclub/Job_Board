bdc20b2142455bcaaff1234fd03b59dc171e1168
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2024-12-26 21:52:07 +0300

    add the location filter and style it

--- js/home.js
+++ js/home.js
@@ -1,15 +1,34 @@
 "use strict";
 
-const dropdown = document.querySelector(".dropdown-header__box");
-const upArrow = document.querySelector(".icon-up");
-const downArrow = document.querySelector(".icon-down");
-const dropdownContent = document.querySelector(".job-type__box");
+const dropdownType = document.querySelector(".dropdown-header__box-type");
+const dropdownLocation = document.querySelector(
+  ".dropdown-header__box-location"
+);
+const typeUpArrow = document.querySelector(".icon-up-type");
+const typeDownArrow = document.querySelector(".icon-down-type");
+
+const locationUpArrow = document.querySelector(".icon-up-location");
+const locationDownArrow = document.querySelector(".icon-down-location");
+
+const dropdownTypeContent = document.querySelector(".job-type__box-type");
+const dropdownLocationContent = document.querySelector(
+  ".job-type__box-location"
+);
 
 //////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////
 
-dropdown.addEventListener("click", function () {
-  upArrow.classList.toggle("icon-visible");
-  downArrow.classList.toggle("icon-visible");
-  dropdownContent.classList.toggle("job-type__box--active");
+const toggleDropdown = function (btnUp, btnDown, dc) {
+  btnUp.classList.toggle("icon-visible");
+  btnDown.classList.toggle("icon-visible");
+  dc.classList.toggle("job-type__box--active");
+};
+
+dropdownType.addEventListener("click", function () {
+  console.log("Hi");
+
+  toggleDropdown(typeUpArrow, typeDownArrow, dropdownTypeContent);
+});
+dropdownLocation.addEventListener("click", function () {
+  toggleDropdown(locationUpArrow, locationDownArrow, dropdownLocationContent);
 });

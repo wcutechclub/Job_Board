e1294e845a20fdfec58f01d2f943c24bc48a1e4c
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2024-12-26 23:12:52 +0300

    make the job cards element dynamic

--- js/home.js
+++ js/home.js
@@ -15,6 +15,8 @@ const dropdownLocationContent = document.querySelector(
   ".job-type__box-location"
 );
 
+const jobCard = document.querySelector(".job-cards--container");
+
 //////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////
 
@@ -32,3 +34,105 @@ dropdownType.addEventListener("click", function () {
 dropdownLocation.addEventListener("click", function () {
   toggleDropdown(locationUpArrow, locationDownArrow, dropdownLocationContent);
 });
+
+////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+const data = {
+  title: "Software Development",
+  catagory: "Information Technology",
+  type: "Full-Time",
+  salary: "70,000$ - 100,000$",
+  location: "Remote",
+  salaryType: "Fixed",
+  description:
+    "Develop and maintain user-facing | | features for a web application | | using React.js.",
+};
+const data1 = {
+  title: "IS",
+  catagory: "Information Technology",
+  type: "Full-Time",
+  salary: "70,000$ - 100,000$",
+  location: "Remote",
+  salaryType: "Fixed",
+  description:
+    "Develop and maintain user-facing | | features for a web application | | using React.js.",
+};
+const data2 = {
+  title: "Health",
+  catagory: "Health Care",
+  type: "Full-Time",
+  salary: "8,000$ - 10,000$",
+  location: "On-Site",
+  salaryType: "Fixed",
+  description:
+    "Develop and maintain user-facing | | features for a web application | | using React.js.",
+};
+const data3 = {
+  title: "Accounting",
+  catagory: "Information Technology",
+  type: "Full-Time",
+  salary: "70,000$ - 100,000$",
+  location: "Remote",
+  salaryType: "Fixed",
+  description:
+    "Develop and maintain user-facing | | features for a web application | | using React.js.",
+};
+const data4 = {
+  title: "Front End Development",
+  catagory: "Information Technology",
+  type: "Full-Time",
+  salary: "70,000$ - 100,000$",
+  location: "Remote",
+  salaryType: "Fixed",
+  description:
+    "Develop and maintain user-facing | | features for a web application | | using React.js.",
+};
+
+const dataArr = [data, data1, data2, data3, data4];
+
+const displayJobs = function (data) {
+  data.forEach(function (mov, i) {
+    const html = `
+  <div class="job-cards flex-col">
+            <div class="flex-container">
+              <span class="light-text date">Posted on</span>
+              <span class="post-date date light-text">Dec 1</span>
+            </div>
+            <h2 class="job-title card-text">
+             ${mov.title}
+            </h2>
+            <div class="flex-container">
+              <span class="job-category card-text">${mov.catagory}</span>
+
+              <div class="flex-container">
+                <span class="card-text salary salary-type light-text"
+                  >${mov.salaryType} -</span
+                >
+                <span class="salary-range salry card-text light-text"
+                  > ${mov.salary}</span
+                >
+              </div>
+            </div>
+            <div class="flex-container">
+              <div class="flex-container icon--text">
+                <ion-icon class="cards-icon" name="location-outline"></ion-icon>
+                <span class="job-location card-text">${mov.location}</span>
+              </div>
+              <div class="flex-container type">
+                <span class="card-text type">Type -</span>
+                <span class="job-type card-text">${mov.type}</span>
+              </div>
+            </div>
+
+            <p class="description card-text">
+            ${mov.description}
+            </p>
+
+            <a class="btn apply-btn" href="#">Apply</a>
+          </div>
+`;
+    jobCard.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+displayJobs(dataArr);

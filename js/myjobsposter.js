774125e149254c2e72ed51f509706d62bde577f2
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-01-31 17:31:44 +0300

    select all the classes of the filter dropdown

---
+++ js/myjobsposter.js
@@ -0,0 +1,16 @@
+const filterBox = document.querySelector(".filter-dropdown--container");
+const filterActive = document.querySelector(".active");
+const filterClose = document.querySelector(".close");
+const filterAll = document.querySelector(".all");
+const drFilterActive = document.querySelector(".activedr");
+const drFilterClose = document.querySelector(".closedr");
+const drFilterAll = document.querySelector(".alldr");
+
+const sortBox = document.querySelector(".sort-dropdown--container");
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+const dropdown = function () {};
+
+filterBox.addEventListener("click", function () {});

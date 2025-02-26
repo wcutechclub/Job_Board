6725eba6814df44a697231500a53f44b7531fec8
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-02-26 09:52:36 +0300

    select the job card elements in js

---
+++ js/myjobsseeker.js
@@ -0,0 +1,12 @@
+const filterBox = document.querySelector(".filter-dropdown--container");
+const filterDropdown = document.querySelector(".filter-dropdown");
+
+const sortBox = document.querySelector(".sort-dropdown--container");
+const sortDropdown = document.querySelector(".sort-dropdown");
+
+const iconUpFilter = document.querySelector(".icon-up-filter");
+const iconDownFilter = document.querySelector(".icon-down-filter");
+const iconUpSort = document.querySelector(".icon-up-sort");
+const iconDownSort = document.querySelector(".icon-down-sort");
+// Card
+const jobCard = document.querySelector(".job-cards--container");

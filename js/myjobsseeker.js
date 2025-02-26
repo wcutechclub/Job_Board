35b7f4ae5b0712d807e84785264929704e012f2b
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-02-26 10:15:59 +0300

    change dynamicaly the background color of the status based on value

--- js/myjobsseeker.js
+++ js/myjobsseeker.js
@@ -10,3 +10,41 @@ const iconUpSort = document.querySelector(".icon-up-sort");
 const iconDownSort = document.querySelector(".icon-down-sort");
 // Card
 const jobCard = document.querySelector(".job-cards--container");
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////// Styles //////////////////////////////////////////////////////////////
+const dropdown = function (dropdownBtn, iconUp, iconDown) {
+  dropdownBtn.classList.toggle("dropdown__box--active");
+  iconUp.classList.toggle("hide-icon");
+  iconDown.classList.toggle("hide-icon");
+};
+
+const statusChangeColor = function (statusContent) {
+  if (statusContent) {
+    if (statusContent.textContent === "Accepted") {
+      statusContent.classList.remove("status-reject");
+      statusContent.classList.remove("status-pen");
+      statusContent.classList.add("status-accept");
+    }
+    if (statusContent.textContent === "Rejected") {
+      statusContent.classList.remove("status-accept");
+      statusContent.classList.remove("status-pen");
+      statusContent.classList.add("status-reject");
+      return;
+    }
+
+    if (statusContent.textContent === "Pending") {
+      statusContent.classList.remove("status-accept");
+      statusContent.classList.remove("status-reject");
+      statusContent.classList.add("status-pen");
+      return;
+    }
+  }
+};
+
+// Change the background color of the status
+const statusContent = document.querySelector(".status");
+console.log(statusContent.textContent);
+// const newStatus = jobCard.lastElementChild.querySelector(".status");
+statusChangeColor(statusContent);

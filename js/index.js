ad57b33039c09633ad2ad8ed4223faa0599036e4
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-03-01 17:01:21 +0300

    add smooth scroll to the page nav bar links

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 const header = document.querySelector(".header");
 const nav = document.querySelector(".nav-bar--container");
 const main = document.querySelector(".main");
+const btnScroll = document.querySelector(".scroll-btn");
 
+// Sticky Nav
 const navHeight = nav.getBoundingClientRect().height;
-let mainHeight = main.getBoundingClientRect().height;
-main.style.height = `${(mainHeight += navHeight)}px`;
-
 const stickNav = function (entries) {
   const [entry] = entries;
   if (!entry.isIntersecting) nav.classList.add("sticky");
@@ -19,6 +18,7 @@ const headerObserver = new IntersectionObserver(stickNav, {
 });
 
 headerObserver.observe(header);
+
 // Reveal Sections
 const allSections = document.querySelectorAll(".section");
 const revealSection = function (entries, observer) {
@@ -37,3 +37,23 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
 });
+
+// Smooth Scroll
+// btnScroll.addEventListener('click', function(e) {
+//   const coords =
+// })
+
+document
+  .querySelector(".nav-btns--container")
+  .addEventListener("click", function (e) {
+    if (e.target.classList.contains("nav-bar--btns")) {
+      e.preventDefault();
+      const id = e.target.getAttribute("href");
+      const targetEl = document.querySelector(id);
+      const targetPos = targetEl.offsetTop - navHeight;
+      window.scrollTo({
+        top: targetPos,
+        behavior: "smooth",
+      });
+    }
+  });
